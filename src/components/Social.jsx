ebcb8e686fbd56f70152a9b2fbbc534b5a0e929d
Antonio290394 <122730142+AntonioCaputo29@users.noreply.github.com>
2023-12-13 17:26:03 +0100

    cv

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,7 +6,7 @@ import Linkedin from '../assets/img/Linkedin.png';
 import Facebook from '../assets/img/Facebook.png';
 import Instagram from '../assets/img/Instagram.png';
 import Github from '../assets/img/Github.png';
-import CV from '../assets/img/cv.png';
+import cv from '../assets/img/cv.png';
 
 
 function Contatti() {
